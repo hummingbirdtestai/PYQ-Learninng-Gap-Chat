ca9c20fef80a2dca3b1576b4033ab49ab1ce9e06
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-26 20:07:35 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,1 +1,9 @@
 // Auth Routes
+const express = require('express');
+const router = express.Router();
+const { startOTP, verifyOTP } = require('../controllers/auth.controller');
+
+router.post('/otp/start', startOTP);
+router.post('/otp/verify', verifyOTP);
+
+module.exports = router;

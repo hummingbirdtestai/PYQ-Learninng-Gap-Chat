b40c5b5295d3863baa52d84f20b364f96f9b1dca
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-26 19:38:50 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,1 +1,12 @@
 // User Routes
+const express = require('express');
+const router = express.Router();
+const { registerUser, getUserById } = require('../controllers/user.controller');
+
+// Register new student
+router.post('/register', registerUser);
+
+// Get user profile by ID
+router.get('/:id', getUserById);
+
+module.exports = router;

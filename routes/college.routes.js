83ed263a80d2e0c05b64f4288641fea96eb9d681
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-27 12:24:03 +0530

    Update college.routes.js

--- routes/college.routes.js
+++ routes/college.routes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { getAllColleges, getCollegeById } = require('../controllers/college.controller');
 
-// GET all colleges
+const {
+  getAllColleges,
+  getCollegeById,
+  createCollege
+} = require('../controllers/college.controller');
+
+// ✅ GET all colleges
 router.get('/', getAllColleges);
 
-// GET one college by ID
+// ✅ GET one college by ID
 router.get('/:id', getCollegeById);
+
+// ✅ POST a new college
+router.post('/', createCollege);
 
 module.exports = router;

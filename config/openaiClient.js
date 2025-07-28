ccd01e03cf64e5f7242f5382457f311104235233
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-28 18:29:33 +0530

    Update openaiClient.js

--- config/openaiClient.js
+++ config/openaiClient.js
@@ -1,1 +1,12 @@
 // OpenAI Client Config
+const { Configuration, OpenAIApi } = require('openai');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+module.exports = { openai };

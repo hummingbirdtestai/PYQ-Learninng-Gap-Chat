05f35e81cf5029e39f4b1189120929b5cbe693d4
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-30 21:11:42 +0530

    Update mcq.routes.js

--- routes/mcq.routes.js
+++ routes/mcq.routes.js
@@ -1,1 +1,9 @@
 // MCQ Routes
+const express = require('express');
+const router = express.Router();
+const { generateMCQGraphFromInput } = require('../controllers/mcq.controller');
+
+// POST /mcqs/generate-from-input
+router.post('/mcqs/generate-from-input', generateMCQGraphFromInput);
+
+module.exports = router;

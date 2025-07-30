f7e525215dfb109a7c6319b4bd0f1b004fa49d33
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-30 19:19:02 +0530

    Update mcqGenerator.controller.js

--- controllers/mcqGenerator.controller.js
+++ controllers/mcqGenerator.controller.js
@@ -1,5 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
-const { supabase } = require('../config/supabaseClient');
+const supabase = require('../config/supabaseClient');
 const openai = require('../config/openaiClient');
 
 const PROMPT_TEMPLATE = `🚨 OUTPUT RULES:

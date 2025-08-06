f09e4bd3f8a2d2942e96d2976878adef2a2e10ee
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-08-06 15:30:15 +0530

    Update mcq.controller.js

--- controllers/mcq.controller.js
+++ controllers/mcq.controller.js
@@ -503,7 +503,7 @@ exports.generatePrimaryMCQs = async (req, res) => {
       .from('mcq_bank')
       .select('id, mcq, correct_answer')
       .is('primary_mcq', null)
-      .limit(50);
+      .limit(5);
 
     if (fetchError) throw fetchError;
 

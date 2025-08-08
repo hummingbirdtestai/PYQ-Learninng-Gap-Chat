f35761097e90ed2ca89475dc14546136be1e3b57
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-08-08 14:29:38 +0530

    Delete routes/classifier.routes.js

--- routes/classifier.routes.js
+++
@@ -1,8 +0,0 @@
-// routes/classifier.routes.js
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/classifier.controller');
-
-router.post('/classify/subjects/run', ctrl.classifySubjectsRun);
-
-module.exports = router;

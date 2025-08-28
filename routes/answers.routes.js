2082c56710870641a563242609a43170a480df0e
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-08-28 19:33:46 +0530

    Update answers.routes.js

--- routes/answers.routes.js
+++ routes/answers.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { saveAnswer } = require("../controllers/answers.controller");
 
+// Save student answer
 router.post("/answers", saveAnswer);
 
 module.exports = router;
